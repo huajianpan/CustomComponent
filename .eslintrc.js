74aa0d953716b4153308923a6b0eeec5100cd498
panjianhua <97274247@qq.com>
2020-09-15 23:35:57 +0800

    commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-trailing-spaces':0,
-    'no-multiple-empty-lines':0
+    'no-multiple-empty-lines':0,
+    '--fix':0
   }
 }
--- src/components/ShowLoading/ShowLoading.vue
+++ src/components/ShowLoading/ShowLoading.vue
@@ -1,28 +1,13 @@
 <template>
-  <section>
-    <div class="loader">
-      <span style="--i:1;"></span>
-      <span style="--i:2;"></span>
-      <span style="--i:3;"></span>
-      <span style="--i:4;"></span>
-      <span style="--i:5;"></span>
-      <span style="--i:6;"></span>
-      <span style="--i:7;"></span>
-      <span style="--i:8;"></span>
-      <span style="--i:9;"></span>
-      <span style="--i:10;"></span>
-      <span style="--i:11;"></span>
-      <span style="--i:12;"></span>
-      <span style="--i:13;"></span>
-      <span style="--i:14;"></span>
-      <span style="--i:15;"></span>
-      <span style="--i:16;"></span>
-      <span style="--i:17;"></span>
-      <span style="--i:18;"></span>
-      <span style="--i:19;"></span>
-      <span style="--i:20;"></span>
+<div>
+  <section :style="{width:width,height:height}">
+    <div :class="{'loader':true,'hasTitle':title.trim()!=''}">
+      <span v-for="i in 20" :style="{'--g':i}" :key="i"></span>
     </div>
+  <div class="text" v-text="title" v-if="title.trim()!=''"></div>
   </section>  
+    <div class="mask" :style="{display:visible?'block':'none'}"></div>
+  </div>
 </template>
 <script>
 export default {
@@ -32,23 +17,44 @@ export default {
     color: {
       type: String, // 颜色参数类型
       default: '#000' // 默认黑色
+    },
+    title: {
+      type: String, 
+      default: '' 
+    },
+    width: {
+      type: String, 
+      default: '120px' 
+    },
+    height: {
+      type: String, 
+      default: '120px' 
+    }
+  },
+  data () {
+    return {
+      visible: 0
     }
   }
 }
 </script>
-<style scoped>
+<style>
 *{
   margin:0;
   padding:0;
-  box-sizing: border-box;
+  /* box-sizing: border-box; */
 }
 section{
+  z-index: 99999;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
   border-radius: 0.6em;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  /* min-height: 100vh; */
-  /* background: #042104; */
   background: rgba(25, 50, 0, 0.8);
   animation: animateBg 10s linear infinite;
 }
@@ -65,13 +71,25 @@ section .loader{
   height: 20%;
   width: 20%;
 }
+.hasTitle{
+  /* margin-top: 10%; */
+}
+section .text{
+  position: relative;
+  color: transparent;
+  margin-top: 20%;
+  background-clip: text;
+  animation: animateBg 10s linear infinite;
+  text-shadow:0 0 0.5px  #00ff0a;
+  /* background-image: linear-gradient(90deg,rgba(0, 255, 0, 1),rgb(255, 0, 255,1)); */
+}
 section .loader span{
   position: absolute;
   top:0;
   left:0;
   width: 100%;
   height: 100%;
-  transform: rotate(calc(18deg*var(--i)))
+  transform: rotate(calc(18deg*var(--g)))
 }
 section .loader span::before{
   content:'';
@@ -82,22 +100,31 @@ section .loader span::before{
   height:12.5%;
   border-radius:50%;
   background: #00ff0a;
-  box-shadow: 0 0 10px  #00ff0a,
-              0 0 20px  #00ff0a,
+  box-shadow: 0 0 10px  #00ff0a;
+              /* 0 0 20px  #00ff0a,
               0 0 40px  #00ff0a,
               0 0 60px  #00ff0a,
-              0 0 80px  #00ff0a,
-              0 0 100px  #00ff0a;
-  animation: animate 2s linear infinite;
-  animation-delay: calc(0.1s * var(--i))
+              0 0 80px  #00ff0a, */
+              /* 0 0 100px  #00ff0a; */
+  animation: animate 1.2s linear infinite;
+  animation-delay: calc(0.1s)
+  /* animation-delay: calc(0.1s * var(--g) ) */
 }
 @keyframes animate {
-  0%{
+  0%,40%{
     transform: scale(1)
   }
-  80%,100%{
+  100%{
     transform: scale(0)
   }
+}
+.mask{
+  position: fixed;
+  top: 0;
+  left: 0;
+  /* background-color: red; */
+  width: 100vw;
+  height: 100vh;
 }
   /* <button class="btn" :style="{color:color}">
     <slot/>
